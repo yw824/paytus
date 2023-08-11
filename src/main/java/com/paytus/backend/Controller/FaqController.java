297a812dff8dbe1fb4e5741ae54ddf97a8f9a4de
seonghoon <shee5032@naver.com>
2023-08-11 17:40:27 +0900

    FaqController_test

---
+++ src/main/java/com/paytus/backend/Controller/FaqController.java
@@ -0,0 +1,52 @@
+package com.paytus.backend.Controller;
+
+import com.paytus.backend.dto.FaqDTO;
+import com.paytus.backend.service.FaqService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.security.PublicKey;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/faq")
+public class FaqController {
+
+
+    private FaqService service;
+
+    public FaqController(FaqService service){
+        this.service = service;
+    }
+
+    @GetMapping("")
+    public List<FaqDTO> getAllFaq() throws Exception {
+        return service.get();
+    }
+
+    @GetMapping("/{faqseq}")
+    public FaqDTO getFaq(@PathVariable("faqseq") int faqseq) throws Exception {
+        return service.get(faqseq);
+    }
+
+    @PostMapping("")
+    public void registerFaq(@RequestBody FaqDTO faqDTO) throws Exception {
+        service.register(faqDTO);
+        //System.out.println(faqDTO);
+    }
+    // @RequestBody: HTTP 요청의 바디내용을 통째로 자바객체로 변환해서 매핑된 메소드 파라미터로 전달해준다.
+
+    @PutMapping("/{faqseq}")
+    public void modifyFaq(@PathVariable("faqseq") int faqseq, @RequestBody FaqDTO faqDTO) throws Exception {
+        faqDTO.setFaqseq(faqseq);
+        service.modify(faqDTO);
+    }
+
+    @DeleteMapping("/{faqseq}")
+    public void removeFaq(@PathVariable("faqseq") int faqseq) throws Exception {
+        service.remove(faqseq);
+    }
+
+
+}
