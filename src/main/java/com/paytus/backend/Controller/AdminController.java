00bf7bcb7d0e4316105ac30a6151ebe88a245d65
fewolee <chulwoo25@naver.com>
2023-08-14 16:48:41 +0900

    Data,Notice,Admin Controller생성

---
+++ src/main/java/com/paytus/backend/Controller/AdminController.java
@@ -0,0 +1,52 @@
+package com.paytus.backend.Controller;
+
+import com.paytus.backend.dto.AdminDTO;
+import com.paytus.backend.service.AdminService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.security.PublicKey;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/admin")
+public class AdminController {
+
+
+    private AdminService service;
+
+    public AdminController(AdminService service){
+        this.service = service;
+    }
+
+    @GetMapping("")
+    public List<AdminDTO> getAllAdmin() throws Exception {
+        return service.get();
+    }
+
+    @GetMapping("/{adminid}")
+    public AdminDTO getAdmin(@PathVariable("adminid") String adminid) throws Exception {
+        return service.get(adminid);
+    }
+
+    @PostMapping("")
+    public void registerAdmin(@RequestBody AdminDTO adminDTO) throws Exception {
+        service.register(adminDTO);
+        //System.out.println(adminDTO);
+    }
+    // @RequestBody: HTTP 요청의 바디내용을 통째로 자바객체로 변환해서 매핑된 메소드 파라미터로 전달해준다.
+
+    @PutMapping("/{adminid}")
+    public void modifyAdmin(@PathVariable("adminid") String adminid, @RequestBody AdminDTO adminDTO) throws Exception {
+        adminDTO.setAdminid(adminid);
+        service.modify(adminDTO);
+    }
+
+    @DeleteMapping("/{adminid}")
+    public void removeAdmin(@PathVariable("adminid") String adminid) throws Exception {
+        service.remove(adminid);
+    }
+
+
+}
---
+++ src/main/java/com/paytus/backend/Controller/NoticeController.java
@@ -0,0 +1,52 @@
+package com.paytus.backend.Controller;
+
+import com.paytus.backend.dto.NoticeDTO;
+import com.paytus.backend.service.NoticeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.security.PublicKey;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/notice")
+public class NoticeController {
+
+
+    private NoticeService service;
+
+    public NoticeController(NoticeService service){
+        this.service = service;
+    }
+
+    @GetMapping("")
+    public List<NoticeDTO> getAllNotice() throws Exception {
+        return service.get();
+    }
+
+    @GetMapping("/{noticeseq}")
+    public NoticeDTO getNotice(@PathVariable("noticeseq") int noticeseq) throws Exception {
+        return service.get(noticeseq);
+    }
+
+    @PostMapping("")
+    public void registerNotice(@RequestBody NoticeDTO noticeDTO) throws Exception {
+        service.register(noticeDTO);
+        //System.out.println(noticeDTO);
+    }
+    // @RequestBody: HTTP 요청의 바디내용을 통째로 자바객체로 변환해서 매핑된 메소드 파라미터로 전달해준다.
+
+    @PutMapping("/{noticeseq}")
+    public void modifyNotice(@PathVariable("noticeseq") int noticeseq, @RequestBody NoticeDTO noticeDTO) throws Exception {
+        noticeDTO.setNoticeseq(noticeseq);
+        service.modify(noticeDTO);
+    }
+
+    @DeleteMapping("/{noticeseq}")
+    public void removeNotice(@PathVariable("noticeseq") int noticeseq) throws Exception {
+        service.remove(noticeseq);
+    }
+
+
+}
