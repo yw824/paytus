7aba61c9dea88ff0a8ed47c9791e29fb321b1707
seonghoon <shee5032@naver.com>
2023-08-17 16:37:36 +0900

    restcontroller commit2

--- src/main/java/com/paytus/backend/Controller/FaqController.java
+++ src/main/java/com/paytus/backend/Controller/FaqController.java
@@ -6,8 +6,11 @@ import com.paytus.backend.model.response.ListResult;
 import com.paytus.backend.model.response.SingleResult;
 import com.paytus.backend.service.FaqService;
 import com.paytus.backend.service.ResponseService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.*;
 
+@Api(tags = {"Faq API"})
 @RestController
 @RequestMapping("/api/faq")
 public class FaqController {
@@ -21,16 +24,19 @@ public class FaqController {
         this.responseService = responseService;
     }
 
+    @ApiOperation(value = "공지사항 게시글 전체조회 api" ,notes = "조건없이 공지사항 전체글을 조회합니다.")
     @GetMapping("")
     public ListResult<FaqDTO> getAllFaq() throws Exception {
         return responseService.getListResult(service.get());
     }
 
+    @ApiOperation(value = "공지사항 특정 글 조회 api" ,notes = "해당 번호의 공지사항 글을 조회합니다.")
     @GetMapping("/{faqseq}")
     public SingleResult<FaqDTO> getFaq(@PathVariable("faqseq") int faqseq) throws Exception {
         return responseService.getSingleResult(service.get(faqseq));
     }
 
+    @ApiOperation(value = "공지사항 게시글 생성 api" ,notes = "faqseq=0을 넣으면 자동으로 번호가 생성되고 공지사항의 글을 생성합니다.")
     @PostMapping("")
     public SingleResult<FaqDTO> registerFaq(@RequestBody FaqDTO faqDTO) throws Exception {
         if (faqDTO.getFaqseq()==0){
@@ -43,6 +49,7 @@ public class FaqController {
     }
     // @RequestBody: HTTP 요청의 바디내용을 통째로 자바객체로 변환해서 매핑된 메소드 파라미터로 전달해준다.
 
+    @ApiOperation(value = "공지사항 게시글 수정 api" ,notes = "해당 번호의 공지사항 글을 수정합니다.")
     @PutMapping("/{faqseq}")
     public SingleResult<FaqDTO> modifyFaq(@PathVariable("faqseq") int faqseq, @RequestBody FaqDTO faqDTO) throws Exception {
         if (getFaq(faqseq).getData()!=null){
@@ -55,6 +62,7 @@ public class FaqController {
             return responseService.getFailSingleResult();
     }
 
+    @ApiOperation(value = "공지사항 게시글 삭제 api" ,notes = "해당 번호의 공지사항 글을 조회합니다.")
     @DeleteMapping("/{faqseq}")
     public CommonResult removeFaq(@PathVariable("faqseq") int faqseq) throws Exception {
         if (getFaq(faqseq).getData()!=null){
