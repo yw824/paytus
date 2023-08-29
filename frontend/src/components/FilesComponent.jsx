4914201c5070021ec3320b43b6ca60e8c1d94201
yw824 <yongwoo25@gmail.com>
2023-08-29 15:49:37 +0900

    - feat: 자료실 파일 다운로드 기능 구현

--- frontend/src/components/FilesComponent.jsx
+++ frontend/src/components/FilesComponent.jsx
@@ -4,7 +4,7 @@ import axios from 'axios';
 import '../assets/Modal.css';
 
 const NoticeComponent = () => {
-    const [notices, setNotices] = useState([]);
+    const [data, setData] = useState([]);
     const [modal, setModal] = useState(false);
 
     const toggleModal = () => {
@@ -12,18 +12,27 @@ const NoticeComponent = () => {
     }
 
     useEffect(() => {
-        axios.get('/api/notice')
-            .then(response => setNotices(response.data.list))
+        axios.get('/api/data')
+            .then(response => setData(response.data.list))
             .catch(error => console.log(error))
     }, []);
 
+    const downloadImage = (seq) => {
+        const url = "/api/data/download/"+seq;
+        const download = document.createElement('a');
+
+        download.href = url;
+        download.setAttribute('download', seq);
+        download.setAttribute('type', 'application/json');
+        download.click();
+    }
     return (
         <div className='object-fill max-w-[1240px] min-w-fit mx-auto bg-white text-bold whitespace-nowrap h-fit'>
             <div>
                 <p className='mt-4 text-3xl mx-3 py-3 font-bold text-gray-400'>- 자료실</p>
                 <hr className='text-gray-400 mt-2 mb-2' />
             </div>
-            <p className='mx-3 mt-2 py-3 font-bold'>총 <span className='text-[#BC614B] font-bold'>{notices.length}</span>건</p>
+            <p className='mx-3 mt-2 py-3 font-bold'>총 <span className='text-[#BC614B] font-bold'>{data.length}</span>건</p>
             <br />
             <div className='mt-4 flex-col mx-auto my-auto grid md:grid-cols-5 gap-4 text-center grid-flow-dense bg-gray-300 py-3 px-3 font-bold whitespace-nowrap'>
                 <p className='font-bold col-1'>번호</p>
@@ -32,31 +41,22 @@ const NoticeComponent = () => {
                 <p className='font-bold col-5'>등록일</p>
             </div>
             {
-                notices.map((notice, index) => {
+                data.map((datum, index) => {
+                    // const url = '/api/data/download' + datum.dataseq;
                     return (
-                        <div>
                             <div className=
                                      {(index) % 2 != 0
                                          ? 'mt-4 flex-col mx-auto grid md:grid-cols-5 gap-4 text-center py-3 px-3 font-bold grid-flow-dense'
-                                         : 'mt-4 flex-col mx-auto grid md:grid-cols-5 gap-4 bg-[#BC614B] bg-opacity-70 text-center py-3 px-3 font-bold'
+                                         : 'mt-4 flex-col mx-auto grid md:grid-cols-5 gap-4 text-center py-3 px-3 font-bold grid-flow-dense bg-[#BC614B] bg-opacity-70 '
                                      }
-                                 key={notice.noticeseq} onClick={toggleModal}>
+                                 key={datum.dataseq} onClick={toggleModal}>
                                 <p className='col-1'>{index}</p>
-                                <p className='col-2'>{notice.adminid}</p>
-                                <strong className='col-start-3 col-end-5'>{notice.noticetitle}</strong>
-                                <p className='col-start-5'>{notice.noticedate.slice(0, 10)}</p>
+                                <p className='col-2'>{datum.adminid}</p>
+                                <button className='col-start-3 col-end-5'
+                                        onClick={() => downloadImage(datum.dataseq)}
+                                >{datum.datatitle}</button>
+                                <p className='col-start-5'>{datum.datadate.slice(0, 10)}</p>
                             </div>
-                            {modal && (
-                                <div className='modal' onClick={toggleModal}>
-                                    <div className='overlay'></div>
-                                    <div className='modal-content'>
-                                        <h2>{notice.noticetitle}</h2>
-                                        <p>{notice.noticetext}</p>
-                                        <button className='close-modal' onClick={toggleModal}>CLOSE</button>
-                                    </div>
-                                </div>
-                            )}
-                        </div>
                     )})
             }
         </div>
