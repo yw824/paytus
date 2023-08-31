e89ffc3289dddc01fe35e54bf32c814d58bc2e59
fewolee <chulwoo25@naver.com>
2023-08-31 15:26:36 +0900

    service, mapper 파일 리팩토링

--- src/main/java/com/paytus/backend/service/FaqService.java
+++ src/main/java/com/paytus/backend/service/FaqService.java
@@ -2,7 +2,6 @@ package com.paytus.backend.service;
 
 import com.paytus.backend.dto.FaqDTO;
 import com.paytus.backend.dto.NoticeDTO;
-import com.paytus.backend.frame.MyService;
 import com.paytus.backend.mapper.FaqMapper;
 import com.paytus.backend.mapper.NoticeMapper;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,34 +10,34 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 
 @Service
-public class FaqService implements MyService<Integer, FaqDTO> {
+public class FaqService  {
 
     @Autowired
     FaqMapper mapper;
 
 
-    @Override
+
     public void register(FaqDTO faqDTO) throws Exception {
         mapper.insert(faqDTO);
     }
 
 
-    @Override
+
     public void remove(Integer integer) throws Exception {
         mapper.delete(integer);
     }
 
-    @Override
+
     public void modify(FaqDTO faqDTO) throws Exception {
         mapper.update(faqDTO);
     }
 
-    @Override
+
     public FaqDTO get(Integer integer) throws Exception {
         return mapper.select(integer);
     }
 
-    @Override
+
     public List<FaqDTO> get() throws Exception {
         return mapper.selectAll();
     }
